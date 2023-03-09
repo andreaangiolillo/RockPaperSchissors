189e67c17e111fe78b583f2685d3b68b0edb6144
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-03-09 12:01:23 +0000

    Initial Commit

---
+++ RockPaperSchissors/RockPaperSchissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperSchissorsApp.swift
+//  RockPaperSchissors
+//
+//  Created by Andrea Angiolillo on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperSchissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
